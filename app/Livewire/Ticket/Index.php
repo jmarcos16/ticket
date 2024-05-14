567f777a7e58a682d694c1ae28913ffe5dc7ec0e
Ze Marcos <jose.ferreira@facedigital.com.br>
2024-05-14 14:29:10 -0300

    wip

---
+++ app/Livewire/Ticket/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Ticket;
+
+use App\Models\Ticket;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.ticket.index', [
+            'tickets' => Ticket::all(),
+        ]);
+    }
+}
---
+++ resources/views/livewire/ticket/index.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div class="w-full">
+        <table class="w-full">
+            <tr class="text-sm font-light uppercase">
+                <th>Id</th>
+                <th>Subject</th>
+                <th>Message</th>
+                <th>Status</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+                <th>Action</th>
+            </tr>
+        </table>
+
+    </div>
+</div>
